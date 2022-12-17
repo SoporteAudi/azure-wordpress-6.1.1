08378721f10c520de732522550510dd7c08d5fda
SoporteAudi <soporte@audisoft.com>
2022-12-16 19:49:16 -0500

    configuration for mysql

--- wp-config.php
+++ wp-config.php
@@ -19,19 +19,11 @@
  */
 
 /** Using environment variables for DB connection information */
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_") !== 0) {
-        continue;
-    }
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
+$connectstr_dbhost = 'henutsenserverexp.mysql.database.azure.com';
+$connectstr_dbname = 'sys';
+$connectstr_dbusername = 'henutsen';
+$connectstr_dbpassword = 'H3nu7s3n*';
+
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
