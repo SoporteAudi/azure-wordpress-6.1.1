3751c27e30b35e6cfa1ead1133f8bce98fd408a6
Juan Felipe - AudiSoft Consulting <109098834+SoporteAudi@users.noreply.github.com>
2023-01-03 09:28:09 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
  */
 
 /** Using environment variables for DB connection information */
-$connectstr_dbhost = 'mysqldb-hen-wp-tst-eastus.mysql.database.azure.com';
+$connectstr_dbhost = 'mysqldb-hen-wp-prd-eastus.mysql.database.azure.com';
 $connectstr_dbname = 'henutsendb';
 $connectstr_dbusername = 'henutsen';
 $connectstr_dbpassword = 'H3nu7s3n*';
